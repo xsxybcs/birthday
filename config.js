48115f0988015d5f90b35355a6b4a824872a3c3d
xsxybcs <121554156+xsxybcs@users.noreply.github.com>
2022-12-28 22:04:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -55,7 +55,7 @@ var config = {
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "颜色",
+        bannar_coming: "这干啥的？",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
